b5e10f1f60a81632af77eef9d6512b2a5bf827c4
miv1 <m.iriarte.val@gmail.com>
2019-04-04 19:03:09 -0400

    Adding special cases

--- Jalasoft.Kitti.Mravi/Program.cs
+++ Jalasoft.Kitti.Mravi/Program.cs
@@ -11,15 +11,16 @@ namespace Jalasoft.Kitti.Mravi
             string pipe, endLine;
             double max = 0;
             double div = 0;
-
+            double sum = 0;
+            int i = 0;
             int n = Int32.Parse(Console.ReadLine());
 
             List<int> aEndPipe = new List<int>();
             List<int> bEndPipe = new List<int>();
             List<int> cFlow = new List<int>();
             List<int> tSuperPipe = new List<int>();
-            List<int> arrayAmount = new List<int>();
-            for (int i = 1; i < n; i++)
+            List<double> arrayAmount = new List<double>();
+            for (i = 1; i < n; i++)
             {
                 pipe = Console.ReadLine();
                 string[] split = pipe.Split(new string[] { " " }, StringSplitOptions.None);
@@ -27,7 +28,6 @@ namespace Jalasoft.Kitti.Mravi
                 bEndPipe.Add(Int32.Parse(split[1]));
                 cFlow.Add(Int32.Parse(split[2]));
                 tSuperPipe.Add(Int32.Parse(split[3]));
-
             }
             endLine = Console.ReadLine();
             string[] amountLiquid = endLine.Split(new string[] { " " }, StringSplitOptions.None);
@@ -35,28 +35,78 @@ namespace Jalasoft.Kitti.Mravi
             {
                 arrayAmount.Add(Int32.Parse(item));
             }
-
-            for (int i = 0; i < tSuperPipe.Count; i++)
+            bool sw = false;
+            int index = aEndPipe[n - 2];
+            i = n - 2;
+            while (i > -1)
+            //for (i = n - 2; i > -1; i--)
             {
-                if (tSuperPipe[i] == 1)
+                
+                for (int aux = 0; aux < i + 1; aux++)
                 {
-                    double dd = Convert.ToDouble(cFlow[i]) / 100;
-                    double ee = Math.Sqrt(arrayAmount[i + 1]);
-                    div = ee / dd;
-                    //div = Math.Sqrt(arrayAmount[i + 1])/Convert.ToDouble(cFlow[i])/100;
+                    if (index == aEndPipe[aux])
+                    {
+                        if (tSuperPipe[aux] == 1)
+                            div = (Math.Sqrt(arrayAmount[aux + 1]) / (Convert.ToDouble(cFlow[aux])));
+                        else
+                            div = (arrayAmount[aux + 1] / (Convert.ToDouble(cFlow[aux])));
+                        if (div > max)
+                            max = div;
+                    }
+                }               
+
+                if (index != aEndPipe[i])
+                {
+                    for (int j = 0; j < i + 1; j++)
+                    {
+                        if (bEndPipe[j] == aEndPipe[index])
+                            arrayAmount[j + 1] = max * 100;
+                    }
+                  //  index = aEndPipe[i];
+                    max = 0;
+                    sw = true;
+                }
+                index = aEndPipe[ i];
+ i--;
+
+
+
+                //if (index != aEndPipe[i])
+                //{
+                //    for (int j = 0; j < i + 1; j++)
+                //    {
+                //        if (bEndPipe[j] == aEndPipe[index])
+                //            arrayAmount[j + 1] = max * 100;
+                //    }
+                //    index = aEndPipe[i];
+                //    max = 0;
+                //    sw = true;
+                //}
+
+
+
+                //if (tSuperPipe[i] == 1)
+                //    div = (Math.Sqrt(arrayAmount[i + 1]) / (Convert.ToDouble(cFlow[i])));
+                //else
+                //    div = (arrayAmount[i + 1] / (Convert.ToDouble(cFlow[i])));
+                //if (div > max)
+                //    max = div;
+            }
+            i++;
+            while (i < n - 1 && aEndPipe[i] == 1)
+            {
+                if (sw == true)
+                {
+                    if (tSuperPipe[i] == 1)
+                        sum += Math.Sqrt(arrayAmount[i + 1]);
+                    else
+                        sum += arrayAmount[i + 1];
                 }
                 else
-                {
-                    double aa = arrayAmount[i + 1];
-                    double bb = Convert.ToDouble(cFlow[i]) / 100;
-                    div = aa / bb;
-                    //div = Convert.ToDouble(arrayAmount[i + 1])/ Convert.ToDouble(cFlow[i]) / 100;
-                }
-
-                if (div > max)
-                    max = div;
+                    sum += max * (Convert.ToDouble(cFlow[i]));
+                i++;
             }
-            
+            Console.Write(sum);
         }
     }
 }
